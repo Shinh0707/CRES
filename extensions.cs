36c56b40156c7dd308daf7bc1a1a1b4cb42fc18c
Shin <133930392+Shinh0707@users.noreply.github.com>
2024-06-05 09:44:47 +0900

    Update extensions.cs

--- extensions.cs
+++ extensions.cs
@@ -8,14 +8,10 @@ using System.Runtime.Versioning;
 using System.Threading;
 
 public static class SerializedObjectExt{
-  public static T GetMethod(this SerializedObject sobj,string methodName){
+  public static MethodInfo? GetMethod (this SerializedObject sobj,string name, BindingFlags bindingAttr)
+  {
     var targetObject = property.serializedObject.targetObject;
-    var targetObjectClassType = targetObject.GetType();
-    var field = targetObjectClassType.GetField(property.propertyPath);
-    if (field != null)
-    {
-      var value = field.GetValue(targetObject);
-      Debug.Log(value.s);
-    }
+    Type targetObjectClassType = targetObject.GetType();
+    return targetObjectClassType.GetMethod(name,bindingAttr);
   }
 }
